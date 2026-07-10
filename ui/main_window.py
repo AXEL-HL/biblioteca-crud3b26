dae15c273c389febd2a6f5795ac05a8b49c6b942
AXEL HL <axelherreralira@gmail.com>
2026-07-10 12:48:39 -0600

    creacion de la ventana principal

---
+++ ui/main_window.py
@@ -0,0 +1,97 @@
+import flet as ft
+
+def main_window(page: ft.Page):
+    # Configurar pagina
+    page.title = "Sistema de Gestion de Biblioteca"
+    page.window_width = 1100
+    page.window_height = 700
+    page. padding = 0
+    page.bgcolor = ft.Colors.BLUE_GREY_50
+
+    #Elementos del contenedor principal
+
+    titulo = ft.Text(
+        "Sistema de getion de biblioteca",
+        size = 24,
+        weight = ft.FontWeight.BOLD
+    )
+
+    subtitulo = ft.Text(
+        "Seleccione una opcion del manu",
+        size = 16,
+        color = ft.Colors.BLUE_GREY_600
+    )
+
+    # Creacon del contenedor principal
+    contenido = ft.Container(
+        content = ft.Column(
+            controls = [
+                titulo,
+                subtitulo
+            ],
+            spacing= 10,
+        ),
+        padding = 30,
+        expand = True
+    )
+
+    # Creacion del menu lateral
+    menu_lateral = ft.Container(
+        width= 220,
+        bgcolor = ft.Colors.BLUE_GREY_900,
+        padding = 20,
+        content = ft.Column(
+            controls =[
+                ft.Text(
+                "Biblioteca",
+                size = 22,
+                weight = ft.FontWeight.BOLD,
+                color = ft.Colors.WHITE
+                ),
+                ft.Text(
+                    "Sistema de Gestion",
+                    size = 12,
+                    color = ft.Colors.WHITE
+                ),
+                ft.Text(
+                    "Sistema de Gestion",
+                    size = 12,
+                    color = ft.Colors.WHITE
+                ),
+                ft.Divider(color=ft.Colors.BLUE_GREY_700),
+                #botones
+                ft.ElevatedButton(
+                    "Libros",
+                    icon=ft.Icons.BOOK,
+                    width = 180
+                ),
+                ft.ElevatedButton(
+                    "Usuarios",
+                    icon=ft.Icons.PERSON,
+                    width = 180
+                ),
+                ft.ElevatedButton(
+                    "Prestamos",
+                    icon=ft.Icons.SWAP_HORIZ,
+                    width = 180
+                ),
+                ft.ElevatedButton(
+                    "Devoluciones",
+                    icon=ft.Icons.KEYBOARD_RETURN,
+                    width = 180
+                ),
+            ],
+            spacing=15
+        )
+    )
+
+    # Layout de la pagina
+    layout = ft.Row(
+        controls=[
+            menu_lateral,
+            contenido
+        ],
+        expand=True
+    )
+
+    page.add(layout)
